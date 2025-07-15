dcd019fe12f7238c2926d122e27af01e34660235
manuel <manuelmaiorano01@gmail.com>
2025-07-15 18:35:55 +0200

    add_animations

--- scripts/components/animations.gd
+++ scripts/components/animations.gd
@@ -1,6 +1,7 @@
 extends Node
 class_name AnimationsManagerComponent
 
+@export var character_movement: CharacterMovementComponent
 @export var animation_tree: AnimationTree
 @export var wieldable_component: WieldableComponent
 
@@ -9,4 +10,7 @@ func _ready() -> void:
 	wieldable_component.is_shooting.connect(on_shooting)
 
 func on_shooting():
-	animation_tree["parameters/OneShot/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
+	animation_tree["parameters/shoot/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
+
+func _physics_process(_delta: float) -> void:
+	animation_tree["parameters/movement_blend/blend_position"] = character_movement.character.velocity.length() /character_movement.run_speed
