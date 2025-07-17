c3152924c6038e7f99fcabb05738d4990915f45e
manuel <manuelmaiorano01@gmail.com>
2025-07-17 17:45:39 +0200

    sniper test

---
+++ scenes/test/sniper/sniper_zoom.gd
@@ -0,0 +1,25 @@
+extends Node
+class_name SniperZoomControl
+
+@export_category("Nodes")
+@export var camera: Camera3D
+
+# Adjustable variables
+@export_category("Parameters")
+@export var min_fov: float = 5.0   # Narrowest FOV (highest zoom)
+@export var max_fov: float = 70.0  # Widest FOV (normal view)
+@export var zoom_speed: float = 2.0
+
+var rotation_x := 0.0
+var rotation_y := 0.0
+
+func _ready():
+	camera.fov = max_fov
+
+func _unhandled_input(event):
+    
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			camera.fov = max(min_fov, camera.fov - zoom_speed)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+			camera.fov = min(max_fov, camera.fov + zoom_speed)
--- scripts/components/interactions/stabbable.gd
+++ scripts/components/interactions/stabbable.gd
@@ -6,7 +6,7 @@ class_name Stabbable
 
 
 @export_category("Parameters")
-@export var distance_to_kill: float = 1.0
+@export var distance_to_kill: float = 1.5
 
 func _ready() -> void:
 	agent.set_meta("Stabbable", self)
@@ -14,7 +14,8 @@ func _ready() -> void:
 func raycast_kill(raycast: RayCast3D):
 	if raycast.is_colliding():
 		var collider = raycast.get_collider()
-		if agent.global_position.distance_to(collider.global_position) < distance_to_kill:
+		if agent.global_position.distance_to(collider.global_position) > distance_to_kill:
+			print(agent.global_position.distance_to(collider.global_position))
 			return false
 		if collider.has_meta("HitBoxComponent"):
 			var hitbox = collider.get_meta("HitBoxComponent", null) as HitBoxComponent
