50aaf938c212f1b29f4af52c82be82a9479b7de1
manuel <manuelmaiorano01@gmail.com>
2025-07-18 10:37:01 +0200

    reorganizing

--- scenes/test/patrol_point.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-@export var patrol_point_id: StringName
-
-func _enter_tree() -> void:
-	if Globals.patrol_points.has(patrol_point_id):
-		Globals.patrol_points[patrol_point_id].append(self)
-	else:
-		Globals.patrol_points[patrol_point_id] = [self]
