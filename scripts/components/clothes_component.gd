21e63df5bc18f1e91f8a46a97cbd9bb3418a9edc
mmgms <mmgms0001@gmail.com>
2025-07-25 11:30:07 +0200

    clothes and base mesh

--- scripts/components/clothes_component.gd
+++ scripts/components/clothes_component.gd
@@ -4,13 +4,13 @@ class_name ClothesComponent
 @export_category("Nodes")
 @export var character: CharacterBody3D
 @export var skeleton: Skeleton3D
+@export var clothes_mesh: MeshInstance3D
 
 @export_category("Parameters")
 @export var initial_clothes: ClothesInfo
 @export var changed_clothes_scene: PackedScene = preload("res://scenes/props/ClothesGround.tscn")
 
 var current_clothes: ClothesInfo
-var current_clothes_mesh: Node3D
 
 func _enter_tree() -> void:
 	character.set_meta("ClothesComponent", self)
@@ -22,20 +22,15 @@ func _ready() -> void:
 func change_clothes(clothes: ClothesInfo):
 	if not clothes:
 		current_clothes = null
-		skeleton.remove_child(current_clothes_mesh)
+		clothes_mesh.mesh = null
 		return
-
-	if current_clothes_mesh:
-		skeleton.remove_child(current_clothes_mesh)
 
 	if current_clothes:
 		add_changed_clothes_scene(current_clothes)
 
 	current_clothes = clothes
-	var instance = current_clothes.scene.instantiate()
-	instance.set_skeleton(skeleton)
-	skeleton.add_child(instance)
-	current_clothes_mesh = instance
+	clothes_mesh.mesh = clothes.clothes_mesh
+	clothes_mesh.material_override = clothes.clothes_material
 
 func add_changed_clothes_scene(clothes_info):
 	var instance = changed_clothes_scene.instantiate()
--- scripts/resources/clothes_resource.gd
+++ scripts/resources/clothes_resource.gd
@@ -3,3 +3,5 @@ class_name ClothesInfo
 
 @export var name: StringName
 @export var scene: PackedScene
+@export var clothes_mesh: Mesh
+@export var clothes_material: Material
